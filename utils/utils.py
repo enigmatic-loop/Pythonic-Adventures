f8e9c7412819a00d9424a31965fa3138d2258114
nina <nina.sohn@gmail.com>
2024-02-21 18:25:07 -0800

    Add util functions

---
+++ utils/utils.py
@@ -0,0 +1,30 @@
+import pygame
+from configs import *
+
+def get_background(name):
+  image = pygame.image.load(join("assets", "Background", name))
+  _, _, width, height = image.get_rect()
+  tiles = []
+
+  for i in range(WIDTH // width + 1):
+    for j in range(HEIGHT // height + 1):
+      pos = (i * width, j * height)
+      tiles.append(pos)
+
+  return tiles, image
+
+def draw(window, background, bg_image, player):
+  for tile in background:
+    window.blit(bg_image, tuple(tile))
+
+  player.draw(window)
+  pygame.display.update()
+
+def handle_move(player):
+  keys = pygame.key.get_pressed()
+  player.x_velocity = 0
+
+  if keys[pygame.K_LEFT]:
+    player.move_left(PLAYER_VELOCITY)
+  if keys[pygame.K_RIGHT]:
+    player.move_right(PLAYER_VELOCITY)
